0065d6a63b5c88a1461795b3bedb5e34bd31bf14
HarrowHaus <donald@harrow.haus>
2026-09-18 10:34:37 -0500

    feat: expose POG foundry route

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Binder } from './components/Binder'
 import { GameShell } from './components/GameShell'
 import { StackBuilder } from './components/StackBuilder'
 import { CatalogLab } from './dev/CatalogLab'
+import { FoundryLab } from './dev/FoundryLab'
 import { CombatLab } from './dev/CombatLab'
 import { LootLab } from './dev/LootLab'
 import { PackLab } from './dev/PackLab'
@@ -16,6 +17,7 @@ export function App() {
   if (path === '/stack') return <StackBuilder />
   if (path === '/binder') return <Binder />
   if (path === '/dev/pack') return <PackLab />
+  if (path === '/dev/foundry') return <FoundryLab />
   if (path === '/dev/catalog') return <CatalogLab />
   if (path === '/dev/loot') return <LootLab />
   if (path === '/dev/combat') return <CombatLab />
